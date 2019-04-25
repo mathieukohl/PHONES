a51d6d9307a457f8eada29eab6f614b29f5c0c9d
Mathieu <mathieufavez1996@hotmail.com>
2019-04-25 13:24:32 +0200

    Test synch Github

--- src/GUI_PHONE/MAIN_PHONE.java
+++ src/GUI_PHONE/MAIN_PHONE.java
@@ -5,10 +5,6 @@ import javax.swing.*;
 public class MAIN_PHONE {
     public static void main(String[] args){
 
-        /*Test synchro git*/
-        //test2
-        //test3
-
         JFrame lock = new MDP_SCREEN();
 
         lock.setSize(480, 800);
